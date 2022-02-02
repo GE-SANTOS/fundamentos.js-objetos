5d655608baad3c2f90571eb2601dec6c28dcdd0d
Geovane Santos <geovane_n.s@outlook.com>
2022-02-02 20:32:54 -0300

    tipo de dados e valores

---
+++ acessar-telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Geovane",
+    idade: 35,
+    CPF: "78912345622",
+    email: "geovane@email.com",
+    fones: ["5571996548282", "5571988644622"]
+}
+
+cliente.fones.forEach(fone => console.log(fone));
