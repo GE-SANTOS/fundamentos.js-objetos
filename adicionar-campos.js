5136135f715a9cea6079d8885c0e1d6d9d8bdca5
Geovane Santos <geovane_n.s@outlook.com>
2022-02-02 18:27:03 -0300

    adicionando e alterando

---
+++ adicionar-campos.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome: "Geovane",
+    idade: 35,
+    CPF: "78912345622",
+    email: "geovane@email.com"
+}
+
+console.log(cliente);
+
+cliente.fone = "998855444";
+
+console.log(cliente);
+
+cliente.fone = "998256444";
+
+console.log(cliente);
