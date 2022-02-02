3ed9a87bf79ea014f9fffc037f94842c3d572876
Geovane Santos <geovane_n.s@outlook.com>
2022-02-02 17:33:35 -0300

    tipos de objeto

---
+++ objetos.js
@@ -0,0 +1,11 @@
+const listaCPFs = ["23154786544", "23154786587", "54784786544"];
+
+//const cliente = ["nome", "André", "idade", "36"];
+
+const cliente = {
+    nome: "Geovane",
+    idade: 35,
+    CPF: "78912345622",
+    email: "geovane@email.com"
+
+}
