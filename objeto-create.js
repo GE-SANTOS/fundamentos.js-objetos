ab78fb6ff5c66b080747deb703a524e8bdd2c09a
Geovane Santos <geovane_n.s@outlook.com>
2022-02-02 22:00:37 -0300

    exemplos de objetos

---
+++ objeto-create.js
@@ -0,0 +1,15 @@
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20"
+}
+
+//const objPersonagem2 = objPersonagem;
+//console.log(objPersonagem.nome) -> Gandalf, o Cinzento
+
+//Object.create cria um novo objeto a parti do objPersonagem
+const objPersonagem2 = Object.create(objPersonagem);
+objPersonagem2.nome = "Gandalf, o Cinzento";
+
+console.log(objPersonagem.nome) //Gandalf
+console.log(objPersonagem2.nome) //Gandalf, o Cinzento
