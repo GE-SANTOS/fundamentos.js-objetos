996835c94527dd0d32d1f9a1b293aa8b3e94fec7
Geovane Santos <geovane_n.s@outlook.com>
2022-02-05 15:43:36 -0300

    criando uma classe

---
+++ classe-cliente.js
@@ -0,0 +1,23 @@
+class Cliente {
+    constructor(nome, email, cpf, saldo) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+        this.saldo = saldo;
+    }
+
+    depositarValor() {
+        this.saldo += valor;
+    }
+
+    exibirSaldo() {
+        console.log(this.saldo);
+    }
+
+}
+
+const gil = new Cliente("Geovane", "geovane@gmail.com", "55688977644", 500);
+
+gil.exibirSaldo();
+
+console.log(gil)
